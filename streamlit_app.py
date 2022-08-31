f5d3dd79e4e1013f98f9189d27f537b8cac48668
VitDConsoomer <112567245+VitDConsoomer@users.noreply.github.com>
2022-08-31 08:11:03 -0700

    Update streamlit_app.py
    added dataframe for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
 
 streamlit.title("Who knows how Magnets work? A story of the 1964 Harlem riots")
 streamlit.header('A story in three parts')
 streamlit.text('Part one: Discovering Magnetism and gravity')
 streamlit.text('Part two: Siphoning gas')
 streamlit.text('Part three: Magnetic gas siphons and the impact of the 1972 oil crisis')
+
+streamlit.header('And now, a list of fruits:')
+fruits = pandas.read_csv("fruit_macros.txt")
+streamlit.dataframe(fruits)
