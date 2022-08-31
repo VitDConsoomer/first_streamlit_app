aa7bd291cf31a27107ee7865a4524196e6470883
VitDConsoomer <112567245+VitDConsoomer@users.noreply.github.com>
2022-08-31 08:30:28 -0700

    Update streamlit_app.py
    made fruits ratios avaliable only when clicked

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ streamlit.header('And now, a list of fruits:')
 # Let's put a pick list here so they can pick the fruit they want to include 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
 
 
