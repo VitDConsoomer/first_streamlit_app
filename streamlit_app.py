2b0071d966002eb8ccb9a3bb4725e0d28298ed46
VitDConsoomer <112567245+VitDConsoomer@users.noreply.github.com>
2022-08-31 08:17:58 -0700

    Update streamlit_app.py
    ask and ye shall recieve

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ streamlit.text('Part two: Siphoning gas')
 streamlit.text('Part three: Magnetic gas siphons and the impact of the 1972 oil crisis')
 
 streamlit.header('And now, a list of fruits:')
-fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(fruits)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(fruits.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
